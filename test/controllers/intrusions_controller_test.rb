d7a91e3ecc39e440c839a9dc54be19757a989f36
Steve <rainoftyren@gmail.com>
2014-06-28 13:41:14 -0700

    Implemented test for the IntrusionsController index action.
    This is my first test function, so it's very basic. I know I should implement tests first before the functionality, but I just wanted to write something that would pass so that I'd know I did it right.

--- test/controllers/intrusions_controller_test.rb
+++ test/controllers/intrusions_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class IntrusionsControllerTest < ActionController::TestCase
-  test 'the user views an intrusion' do
-    Intrusion.create description: 'test'
-    get :show
+  test 'index' do
+    get :index
+    assert_equal (Intrusion.all.sort { |a, b| a.id <=> b.id }), assigns(:intrusions), "@intrusions should be set to an array of all intrusions sorted by id."
   end
 end
