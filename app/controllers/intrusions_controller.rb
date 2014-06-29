7b11c78d69a7ecaa4b164ace37dbae0c214f2d22
Steve <rainoftyren@gmail.com>
2014-06-29 15:46:10 -0700

    Implemented create action of Intrusion controller

--- app/controllers/intrusions_controller.rb
+++ app/controllers/intrusions_controller.rb
@@ -5,10 +5,17 @@ class IntrusionsController < ApplicationController
   end
   
   def create
+    @intrusion = Intrusion.create intrusion_params
     
+    redirect_to @intrusion
   end
   
   def new
     @intrusion = Intrusion.new
   end
+  
+  private
+    def intrusion_params
+      params.require(:intrusion).permit(:description)
+    end
 end
