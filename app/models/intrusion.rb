15b6ffac1395a6baa286a052ed19f140c6814637
Steve <rainoftyren@gmail.com>
2014-07-01 15:53:30 -0700

    Added Tag model and controller.

--- app/models/intrusion.rb
+++ app/models/intrusion.rb
@@ -1,3 +1,4 @@
 class Intrusion < ActiveRecord::Base
   searchkick
+  has_and_belongs_to_many :tags
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :intrusions do
     get 'search', on: :collection
   end
+  resources :tags
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
