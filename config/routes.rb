ea1a255dd44799dbd2c79cb56bea5bdef09e23db
Steve <rainoftyren@gmail.com>
2014-07-12 12:01:49 -0700

    Added Pages controller and disclaimer view.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   end
   resources :tags
   
+  
+  get '/:id', to: 'pages#show'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
