717a1ed272792593bc37f648eaec66286f62f35e
Steve <rainoftyren@gmail.com>
2014-07-14 12:53:00 -0700

    Work in progress.

--- app/assets/javascripts/tags.js.coffee
+++ app/assets/javascripts/tags.js.coffee
@@ -1,3 +1,13 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$('#newTagLink').on 'click', (e) ->
+  e.preventDefault()
+  $.ajax
+    url: '/tags/new.json'
+    success: (data) ->
+      window.mydata = data
+      # $('.intrusion-div').first().html data.html
+    error: (data) ->
+      console.debug data
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,38 +1,44 @@
 class TagsController < ApplicationController
   before_filter :find_tag, only: [:show, :edit, :update, :destroy]
   before_filter :new_tag, only: [:new, :create]
-  
+
   def index
     @tags = Tag.order :name
   end
-  
+
   def new
     @form_action = :create
+    respond_to do |format|
+      format.html
+      format.json {
+        render json: { html: render_to_string(partial: 'tag_form', formats: [:html]), s: 's'}
+      }
+    end
   end
-  
+
   def create
     create_or_update :create
   end
-  
+
   def show
     @intrusions = @tag.intrusions.sort
   end
-  
+
   def edit
     @form_action = :update
   end
-  
+
   def update
     create_or_update :update
   end
-  
+
   def destroy
     @tag.destroy
     redirect_to :tags
   end
-  
+
   private
-  
+
   def create_or_update(method)
     @tag.update(tag_params)
     if @tag.valid?
@@ -42,20 +48,20 @@ class TagsController < ApplicationController
       render_with_error ({create: :new, update: :edit}[method]), @tag
     end
   end
-  
+
   def render_with_error(action, tag)
     @error_msg = tag.errors.map { |_,m| m }
     render action
   end
-  
+
   def tag_params
     params.require(:tag).permit(:name)
   end
-  
+
   def find_tag
     @tag = Tag.find(params[:id])
   end
-  
+
   def new_tag
     @tag = Tag.new
   end
