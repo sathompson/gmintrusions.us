80929b9e151d73a05ee9e793823c0034aaf2072d
Steve <rainoftyren@gmail.com>
2014-07-15 13:39:44 -0700

    Updated, fixed, and made more consistent tests.

--- test/controllers/intrusions_controller_test.rb
+++ test/controllers/intrusions_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class IntrusionsControllerTest < ActionController::TestCase
+
+  def self.startup
+    Rake::Task['db:reset'].invoke
+  end
+
   test ':index sets @intrusions to a sorted list of all intrusions' do
     get :index
-    assert_equal (Intrusion.all.sort { |a, b| a.id <=> b.id }),
+    assert_equal (Intrusion.all.sort { |a, b| a.created_at <=> b.created_at }),
       assigns(:intrusions),
       '@intrusions should be set to an array of all intrusions sorted by id.'
   end
-  
+
   test ':create creates a new intrusion' do
-    testDescription = 'eoseufnisenc'
+    testDescription = rand.to_s
     post :create, intrusion: { description: testDescription }
-    
+
     assert_not_nil Intrusion.find_by(description: testDescription),
       'new intrusion not found'
   end
-  
+
   test 'does not save without description' do
     [ nil, '' ].each do |testDescription|
       post :create, intrusion: { description: testDescription }
@@ -24,42 +29,42 @@ class IntrusionsControllerTest < ActionController::TestCase
         'intrusion with empty description found'
     end
   end
-  
+
   test ':new instantiates a new intrusion' do
     get :new
     assert_not_nil assigns(:intrusion), 'new intrusion not present'
   end
-  
+
   test ':edit sets @intrusion to the correct intrusion' do
-    testDescription = 'sekufbuvb'
+    testDescription = rand.to_s
     intrusion = Intrusion.new
     intrusion.description = testDescription
     intrusion.save
-    
+
     get :edit, id: intrusion.id
     assert_equal intrusion, assigns(:intrusion),
       'the wrong intrusion is displayed'
   end
-  
+
   test ':show sets @intrusion to the right intrusion' do
-    testDescription = 'weubcukdbiu'
+    testDescription = rand.to_s
     intrusion = Intrusion.new
     intrusion.description = testDescription
     intrusion.save
-    
+
     get :show, id: intrusion.id
     assert_equal intrusion, assigns(:intrusion),
       'the wrong intrusion is displayed'
   end
-  
+
   test ':update updates the correct intrusion' do
     [:patch, :put].each do |http_method|
-      testDescription = 'weoindsuc'
+      testDescription = rand.to_s
       intrusion = Intrusion.new
       intrusion.description = testDescription
       intrusion.save
 
-      testUpdatedDescription = 'hskurncusn'
+      testUpdatedDescription = rand.to_s
       self.send(http_method, :update, id: intrusion.id,
         intrusion: { id: intrusion.id, description: testUpdatedDescription })
       assert_equal intrusion, assigns(:intrusion),
@@ -68,38 +73,38 @@ class IntrusionsControllerTest < ActionController::TestCase
         'intrusion does not have the updated description'
     end
   end
-  
+
   test ':destroy destroys the correct intrusion' do
-    testDescription = 'jpionftygubhjkn'
+    testDescription = rand.to_s
     intrusion = Intrusion.new
     intrusion.description = testDescription
     intrusion.save
-    
+
     delete :destroy, id: intrusion.id
     assert_empty Intrusion.where(id: intrusion.id),
       'the intrusion was not deleted'
   end
-  
+
   test ':update adds new tags' do
     num_tags = 3
     [:patch, :put].each do |http_method|
       intrusion = Intrusion.new
-      intrusion.description = Faker::Lorem.sentence
+      intrusion.description = rand.to_s
       intrusion.save
-      
+
       tags = []
       num_tags.times do
         tag = Tag.new
-        tag.name = Faker::Lorem.word
+        tag.name = rand.to_s
         tag.save
         tags << tag
       end
       intrusion.tags.concat tags
-      
+
       test_tag = Tag.new
-      test_tag.name = Faker::Lorem.word
+      test_tag.name = rand.to_s
       test_tag.save
-      
+
       form = {id: intrusion.id, description: intrusion.description,
         tag_ids: (tags << test_tag).map do |tag| tag.id end}
       self.send http_method, :update, id: intrusion.id, intrusion: form
@@ -107,26 +112,26 @@ class IntrusionsControllerTest < ActionController::TestCase
         "intrusion does not have new tag for http method: #{http_method}"
     end
   end
-  
+
   test ':update deletes tags' do
     num_tags = 3
     [:patch, :put].each do |http_method|
       intrusion = Intrusion.new
-      intrusion.description = Faker::Lorem.sentence
+      intrusion.description = rand.to_s
       intrusion.save
-      
+
       tags = []
       num_tags.times do
         tag = Tag.new
-        tag.name = Faker::Lorem.word
+        tag.name = rand.to_s
         tag.save
         tags << tag
       end
       intrusion.tags.concat tags
-      
+
       test_tag = tags[rand num_tags]
       tags.delete test_tag
-      
+
       form = {id: intrusion.id, description: intrusion.description,
         tag_ids: tags.map do |tag| tag.id end}
       self.send http_method, :update, id: intrusion.id, intrusion: form
@@ -134,23 +139,23 @@ class IntrusionsControllerTest < ActionController::TestCase
         "intrusion still has tag for http method: #{http_method}"
     end
   end
-  
+
   test ':create creates new intrusion with tags' do
     num_tags = 3
     tags = []
     num_tags.times do
       tag = Tag.new
-      tag.name = Faker::Lorem.word
+      tag.name = rand.to_s
       tag.save
       tags << tag
     end
-    
-    test_description = Faker::Lorem.sentence
-    
+
+    test_description = rand.to_s
+
     form = {description: test_description,
       tag_ids: tags.map do |tag| tag.id end}
     post :create, intrusion: form
-    
+
     intrusion = Intrusion.find_by(description: test_description)
     tags.each do |tag|
       assert intrusion.tags.include?(tag),
--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -1,25 +1,26 @@
 require 'test_helper'
 
-def self.startup
-  Rake::Task['db:reset'].invoke
-end
-
 class TagsControllerTest < ActionController::TestCase
+
+  def self.startup
+    Rake::Task['db:reset'].invoke
+  end
+
   test ':index sets @tags to a sorted list of all tags' do
     get :index
     assert_equal (Tag.all.sort { |a, b| a.name <=> b.name }),
       assigns(:tags),
       '@tags should be set to an array of all tags sorted by name.'
   end
-  
+
   test ':create creates a new tag' do
-    testName = Faker::Lorem.word
+    testName = rand.to_s
     post :create, tag: { name: testName }
-    
+
     assert_not_nil Tag.find_by(name: testName),
       'new tag not found'
   end
-  
+
   test 'does not save without name' do
     [ nil, '' ].each do |testName|
       post :create, tag: { name: testName }
@@ -28,42 +29,42 @@ class TagsControllerTest < ActionController::TestCase
         'tag with empty description found'
     end
   end
-  
+
   test ':new instantiates a new tag' do
     get :new
     assert_not_nil assigns(:tag), 'new tag not present'
   end
-  
+
   test ':edit sets @tag to the correct tag' do
-    testName = Faker::Lorem.word
+    testName = rand.to_s
     tag = Tag.new
     tag.name = testName
     tag.save
-    
+
     get :edit, id: tag.id
     assert_equal tag, assigns(:tag),
       'the wrong tag is displayed'
   end
-  
+
   test ':show sets @tag to the right tag' do
-    testName = Faker::Lorem.word
+    testName = rand.to_s
     tag = Tag.new
     tag.name = testName
     tag.save
-    
+
     get :show, id: tag.id
     assert_equal tag, assigns(:tag),
       'the wrong tag is displayed'
   end
-  
+
   test ':update updates the correct tag' do
     [:patch, :put].each do |http_method|
-      testName = Faker::Lorem.word
+      testName = rand.to_s
       tag = Tag.new
       tag.name = testName
       tag.save
 
-      testUpdatedName = Faker::Lorem.word
+      testUpdatedName = rand.to_s
       self.send(http_method, :update, id: tag.id,
         tag: { id: tag.id, name: testUpdatedName })
       assert_equal tag, assigns(:tag),
@@ -72,13 +73,13 @@ class TagsControllerTest < ActionController::TestCase
         'tag does not have the updated name'
     end
   end
-  
+
   test ':destroy destroys the correct tag' do
-    testName = Faker::Lorem.word
+    testName = rand.to_s
     tag = Tag.new
     tag.name = testName
     tag.save
-    
+
     delete :destroy, id: tag.id
     assert_empty Tag.where(id: tag.id),
       'the tag was not deleted'
